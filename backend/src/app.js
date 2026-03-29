4aa5adce4ebb14c42e2fd6f9abc9ed8d9d342780
Dmitrii <dementievdmitriy.d@gmail.com>
2026-03-30 00:33:30 +0500

    Init working backend setup with Prisma and Sqlite

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,38 @@
-import express from 'express'
-import cors from 'cors'
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 
-const app = express()
+import { PrismaClient } from './generated/prisma/index.js';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-app.use(cors())
-app.use(express.json())
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+const adapter = new PrismaBetterSqlite3({
+  url: "file:./dev.db",
+});
+
+const prisma = new PrismaClient({ adapter });
+
+// тесты
 app.get('/', (req, res) => {
-  res.send('API is running')
-})
+  res.send('API is running');
+});
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json({ success: true,count: users.length, users });
+  } 
+  catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+  
+});
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000')
-})
+  console.log('Server running on port 3000');
+});
